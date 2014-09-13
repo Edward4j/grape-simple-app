c274f35bd969e593180e7ecd0226d3f6c14e2003
Oded Arbel <oded@showbox.com>
2014-09-13 14:26:25 +0300

    setup Application::app for config.ru

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,10 @@ Bundler.require :default, ENV['RACK_ENV']
  
 require "#{Application::Path::api}/#{Application::name}"
 
-use Rack::TryStatic,
-  :root => "#{Application::Path::client}",
-  :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
- 
-run Legioner::Ping
+Application::app = Rack::Builder.new do
+  use Rack::TryStatic,
+    :root => "#{Application::Path::client}",
+    :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
+
+  run Legioner::Ping
+end
