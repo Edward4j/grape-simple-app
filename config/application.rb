504cea4b192feeffda3bd1f7a81f2e1234484854
Oded Arbel <oded@geek.co.il>
2014-09-15 11:02:18 +0300

    use Grape mount to combine API classes
    This will give the user more control of URL design, but mounting all APIs in the same Rack URL prefix and letting Grape `prefix` control the actual mapping.

--- config/application.rb
+++ config/application.rb
@@ -7,24 +7,24 @@ Application::app = Rack::Builder.new do
     :root => "#{Application::Path::client}",
     :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
 
-  # map API classes to /api URLS
-  Dir["#{Application::Path::api}/**/*.rb"].each do |file|
-    existing_classes = ObjectSpace.each_object(Class).to_a # snapshot current class list before
-    load file # we load the API source file
+  # mount API classes under a single namespace 
+  # (APIs should use :prefix to make sure they don't overlap)
+  class ApplicationAPI < Grape::API
+    # scan api directory for API classes
+    Dir["#{Application::Path::api}/**/*.rb"].each do |file|
+      existing_classes = ObjectSpace.each_object(Class).to_a # snapshot current class list before
+      load file # we load the API source file
     
-    # then figure what API class was loaded
-    clazz = (ObjectSpace.each_object(Class).to_a - existing_classes).
-      find_all(&:name). # get all new class objects
-      select { |c| c < Grape::API }. # ignore non-API classes loaded
-      first # only one API class can be mapped to a single URL
-    # also having multiple classes in a file is bad practice so we don't support that
-    # but if you do that, there's no tell which class will be listed here.
-      
-    next if clazz.nil? # sanity check
-    uripath = "/api/" + file.gsub(/\.rb$/,"").gsub(%r{^#{Application::Path::api}/?},"")
-    puts "Registering #{clazz} for #{uripath}" 
-    map uripath do
-      run clazz
+      # then figure what API class was loaded
+      (ObjectSpace.each_object(Class).to_a - existing_classes).find_all(&:name).each do |clazz|
+        next unless clazz < Grape::API # ignore non-API classes loaded
+        next if clazz.nil? # sanity check
+        mount clazz
+      end
     end
+  end
+  
+  map "/api" do
+    run ApplicationAPI
   end
 end
